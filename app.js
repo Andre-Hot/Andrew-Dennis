c497fea4a30d09c8d673f97c329166af7881ff26
Dennis2300 <124138161+Dennis2300@users.noreply.github.com>
2023-04-27 09:25:36 +0200

    Dennis - update 2

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 "use strict";
 
+window.addEventListener("load", initApp)
+
+function initApp() {
+    console.log("website loaded!");
+    newProject()
+    console.log("newProject called!");
+}
+
 function newProject() {
     console.log("Its working");
 } 
